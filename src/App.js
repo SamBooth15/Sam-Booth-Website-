58159e592b6fccd2d2c7c64269240c1c919d5ea4
SamBooth15 <boothsam1515@gmail.com>
2025-05-15 14:48:26 -0400

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Athletic Training with [Your Name]</h1>
+      <p>Elite training programs for athletes. Basketball, strength, speed, and injury rehab.</p>
+    </div>
+  );
+}
+
+export default App;
